fa983d701e2c4397ce91975455642a7d521f6f6d
Jenish Khunt <jkhunt@ebay.com>
2020-06-29 00:17:31 +0200

    Fixer

--- make_valid_parenthesis.py
+++ make_valid_parenthesis.py
@@ -3,15 +3,26 @@ def makevalid(s: str) -> bool:
     bracket_map = {')':'(','}':'{',']':'['}
     s_list = list(s)
     count = 0
+    indexer = []
     for char in s_list:
         if char in bracket_map:
-            top = stk.pop() if stk else '/'
+            if stk:
+                top = stk.pop()
+                indexer.pop()
+            else:
+                top = '/'
             if bracket_map[char] != top:
                 s_list[count] = ''
         else:
             if char in bracket_map.values():
                 stk.append(char)
+                indexer.append(count)
         count+=1
+        
+    if stk:
+        n = len(stk)
+        for i in range(n):
+            s_list[indexer[i]] = ''
     return ''.join(s_list)
 
-print(makevalid("b(d)ss{}s}a"))
+print(makevalid("b(d){{ss{}s}}}}}{{{{{a"))
