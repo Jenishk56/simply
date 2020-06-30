2ae3b72459365a59c5479cc8adace75c5401e58b
Jenish Khunt <jkhunt@ebay.com>
2020-06-30 23:30:53 +0200

    send email as well

--- 100_server.sh
+++ 100_server.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 list=("saltmaster001" "shellserver001" "consul001" "consul002")
-
+result=''
 for i in ${list[@]}
 do
         ssh $i -p 22 "pgrep consul > /dev/null"
         if [ $? -eq 0 ]
         then
-                echo "Process is running for ${i}."
+                result="${result} Process is running for ${i} \n."
         else
-                echo "Process is not running."
+                result="${result} Process is not running for ${i} \n."
         fi
 done
+
+# echo $result
+echo $array | mail -s "list of the runnin servers" "jkhunt@ebay.com"
